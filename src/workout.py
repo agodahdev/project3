2cf18b0c8c87e7360c1f7ac31461efff6f2412a6
Ahmed Godah <godahahmed@gmail.com>
2024-10-19 15:51:09 +0000

    Add workout class to handle workout logging

---
+++ src/workout.py
@@ -0,0 +1,11 @@
+class Workout:
+    def__init__(self, exercise, duration, intensity, colories_burned, date):
+      self.exercise = exercise
+      self.duration = duration
+      self.intensity = intensity
+      self.colories_burned = calories_burned
+      self.data = data 
+
+      #String for printing workout
+      def__str__(self):
+      return f"{self.date}: {self.exercise} for {self.duration} min {self.intensity} intensity) - {self.calories_burned} kcal"
